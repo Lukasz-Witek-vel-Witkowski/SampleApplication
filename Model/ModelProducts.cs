5d2e4b3338e0f32f8891443cf65445b14e488ab3
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-05-05 11:18:08 +0200

    Add class ModelProducts

---
+++ Model/ModelProducts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication.Model
+{
+    class ModelProducts
+    {
+        private List<Product> Products;
+
+        public ModelProducts()
+        {
+            Products = new List<Product>();
+        }
+        public void addProduct(Product product)
+        {
+            Products.Add(product);
+        }
+        public Product[] getProducts()
+        {
+            return Products.ToArray();
+        }
+    }
+}
