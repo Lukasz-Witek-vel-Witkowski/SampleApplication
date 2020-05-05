e3140d0468f87c5c0eeff53df40def6834cb51fe
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-05-05 11:06:11 +0200

    Adding methods to Product class

--- Product.cs
+++ Product.cs
@@ -16,10 +16,11 @@ namespace SampleApplication
         ulong ID;   //System ID
         string Image;   //Photo address
 
-        public Product(string name, ulong number, ulong id)
+        public Product(string name, ulong number,double price, ulong id)
         {
             Name = name;
             Number = number;
+            Price = price;
             ID = id;
         }
         public Product(string name, ulong number, double price, string description, ulong id, string image)
@@ -30,6 +31,44 @@ namespace SampleApplication
             Description = description;
             ID = id;
             Image = image;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+
+        public ulong getNumber()
+        {
+            return Number;
+        }
+
+        public double getPrice()
+        {
+            return Price;
+        }
+
+        public string getDescription()
+        {
+            return Description;
+        }
+
+        public ulong getID()
+        {
+            return ID;
+        }
+
+        public string getImage()
+        {
+            return Image;
+        }
+
+        public void setImage(string image)
+        {
+            Image = image;
+        }
+        public void setDescription(string description)
+        {
+            Description = description;
         }
     }
 }
