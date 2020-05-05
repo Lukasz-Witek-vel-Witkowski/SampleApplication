e6b9232a7f40e1193f1b34175a21717456458b68
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-05-05 10:58:01 +0200

    Add class product

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication
+{
+    class Product
+    {
+        string Name;    //Product Name
+        ulong Number;   //System number
+        double Price;   //Product Price
+        string Description; //Product Description
+        ulong ID;   //System ID
+        string Image;   //Photo address
+
+        public Product(string name, ulong number, ulong id)
+        {
+            Name = name;
+            Number = number;
+            ID = id;
+        }
+        public Product(string name, ulong number, double price, string description, ulong id, string image)
+        {
+            Name = name;
+            Number = number;
+            Price = price;
+            Description = description;
+            ID = id;
+            Image = image;
+        }
+    }
+}
