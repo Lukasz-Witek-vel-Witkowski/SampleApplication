c209f4c4e2aae3aa30b88c86a8481f4e2e9e2024
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-05-06 10:08:32 +0200

    Update interface SQL

---
+++ SQL/Manager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleApplication.SQL
+{
+    class Manager
+    {
+        private ManagerSQL sql;
+        private string Database;
+        private string User;
+        private string Password;
+        private string Address;
+        public Manager(String Path)
+        {
+            load(Path);
+        }
+
+        private void load(String Path)
+        {
+            if(File.Exists(Path)){
+                StreamReader streamReader = new StreamReader(Path);
+                String s;
+                do
+                {
+                    s = streamReader.ReadLine();
+                    if (s.IndexOf("DataBase") != 0)
+                    {
+                        Database = s.Substring(s.IndexOf(" \""), s.IndexOf("\" "));
+                    }
+                    if (s.IndexOf("Address") != 0)
+                    {
+                        Address = s.Substring(s.IndexOf(" \""), s.IndexOf("\" "));
+                    }
+                    if (s.IndexOf("User") != 0)
+                    {
+                        User = s.Substring(s.IndexOf(" \""), s.IndexOf("\" "));
+                    }
+                    if (s.IndexOf("Password") != 0)
+                    {
+                        Password = s.Substring(s.IndexOf(" \""), s.IndexOf("\" "));
+                    }
+
+                } while (s != null);
+                streamReader.Close();
+            }
+            else
+            {
+                Debug.WriteLine("Error reading configuration file");
+            }
+            sql = new ManagerSQL(Address, Database, User, Password);
+            sql.connect();
+        }
+
+        public ManagerSQL getManagerSQL()
+        {
+            return sql;
+        }
+
+
+    }
+}
---
+++ SQL/SqlTableProducts.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication.SQL
+{
+    public class SqlTableProducts
+    {
+        List<QueryProduct> list;
+       public SqlTableProducts()
+        {
+
+        }
+        public void add(SqlDataReader sqlDataReader)
+        {
+            QueryProduct queryProduct = new QueryProduct();
+            queryProduct.Name = sqlDataReader["Name"].ToString();
+            queryProduct.ID = sqlDataReader["ID"].ToString();
+            queryProduct.Number = sqlDataReader["Number"].ToString();
+            queryProduct.Description = sqlDataReader["Description"].ToString();
+            queryProduct.Price = sqlDataReader["Price"].ToString();
+            queryProduct.Image = sqlDataReader["Image"].ToString();
+            list.Add(queryProduct);
+        }
+
+        public int Size()
+        {
+            return list.Count;
+        }
+       
+        public Product getProduct(int index)
+        {
+         
+            if (list.Count > index) {
+                Product product;
+                QueryProduct queryProduct = list.ElementAt(index);
+                product = new Product(queryProduct.Name,
+                    ulong.Parse(queryProduct.Number),
+                    double.Parse(queryProduct.Price),
+                    queryProduct.Description,
+                    ulong.Parse(queryProduct.ID),
+                    queryProduct.Image);
+                return product;
+            }
+            return null;
+        }
+    }
+
+    internal class QueryProduct
+    {
+        public String Name { get; set; }
+        public String ID { get; set; }
+        public String Description { get; set; }
+        public String Price { get; set; }
+        public String Number { get; set; }
+        public String Image { get; set; }
+       public QueryProduct() { }
+    }
+}
